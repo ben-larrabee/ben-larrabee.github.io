1faf31c96892d9ed8daf93a5a77dbb179fd7cac2
Ben Larrabee <ben.larrabee@interapthq.com>
2016-09-29 12:39:42 -0400

    Finished minus the shareThis

--- Guessing/guessing.js
+++ Guessing/guessing.js
@@ -3,19 +3,96 @@
 var d = []; // the computers three numbers
 var currentGuess = 1; // counts up from 1 to 10, helps place row.
 var g = [0, 1, 2]; // triplet
-var gamewin;
+var gameOver;
 var exactmatch = "panel-success";
 var numbermatch = "panel-warning";
 var nomatch = "panel-danger";
+var newline;
+var replay;
 
 // -------------------------------------------------
 $(document).ready(function(){
   d = computerGuess();
-  //postAlert("I have three numbers in mind, care to guess?");
-  //console.log(d);
+  postAlert("I have three numbers in mind, care to guess?");
   $("#process").click(function(){
-    g = [$("#fDigit").val(), $("#sDigit").val(), $("#tDigit").val()];
-    newline = $("#rowmaker").html();
+    g = [parseInt($("#fDigit").val()), parseInt($("#sDigit").val()), parseInt($("#tDigit").val())]; 
+    for (var i = 0; i < 3; i++){
+      if (g[i] < 0 || g[i] > 9 ) {
+        return postAlert("You have input an invalid number");
+      }
+    }
+    if (g[0] == g[1] || g[0] == g[2] || g[1] == g[2]){
+      postAlert("Heads up!  Your guess was submitted, but it really isn't possible to have duplicates.  Just so you know");
+    }
+    lineMaker(g);    
+    $(".container").append(newline);
+    if (g[0] == d[0] && g[1] == d[1] && g[2] == d[2]) {
+      postAlert("Wow, you won!");
+      floatAlert(d);
+      gameOver = true;
+      // stripped
+    } else {
+      currentGuess++;
+      $("#remaining").html(11 - currentGuess);
+      if (currentGuess == 10) {
+        postAlert("Last chance!!!");
+      }
+      if (currentGuess > 10) {
+        floatAlert(d);
+        gameOver = true;
+         
+        // stripped
+        /* , function(){
+          postPrompt("Would you like to play again?", null, null);
+        } */
+      }
+    }
+    if (gameOver) {
+      playAgain();
+    }
+  });//end click
+});//end documentready
+
+//--------------------------
+// Function definitions
+computerGuess = function(){
+ var digitOne = Math.floor(Math.random()*10);
+ var digitTwo;
+ var digitThree;
+ do {
+   digitTwo = Math.floor(Math.random()*10);
+ } while (digitTwo == digitOne);
+  do {
+   digitThree = Math.floor(Math.random()*10);  
+  } while (digitThree == digitOne || digitThree == digitTwo);
+  return [digitOne, digitTwo, digitThree];
+}
+postAlert = function(someText) {
+//  alert(someText);
+ var alertmessage = $("#alertmaker").html();
+ alertmessage = alertmessage.replace("@@target@@", someText);
+ $(".container").append(alertmessage);
+}
+disableSubmit = function() {
+  $("#process").disabled = true;
+}
+floatAlert = function(share) {
+//  alert(share);
+  lineMaker(share);
+  newline = newline.replace("<li><h4>Guess ", "");
+  newline = newline.replace("</h4></li>", "");
+  postAlert(newline);
+}
+// postPrompt = function(someText, result1, result2){
+//   var answer = prompt(someText);
+//   if (answer[0].toUpperCase() == "Y") {
+//     location.reload();
+//   } else{
+//     location.assign("http://www.hampsterdance.com/classics/interactivedance.htm");
+//   }
+// }
+lineMaker = function(g) {
+  newline = $("#rowmaker").html();
     newline = newline.replace("@@num@@", currentGuess);
     newline = newline.replace("@@first@@", g[0]);
     newline = newline.replace("@@second@@", g[1]);
@@ -50,87 +127,20 @@ $(document).ready(function(){
       newline = newline.replace("@@match3@@", nomatch);
       newline = newline.replace("@@desc3@@", "No Match");
     }      
-    $(".container").append(newline);
-    if (g[0] == d[0] && g[1] == d[1] && g[2] == d[2]) {
-      postAlert("Wow, you won!");
-      disableSubmit();
-      floatAlert(d);
-      postPrompt("Would you like to play again?", null, null);
-    } else {
-      currentGuess++;
-      if (currentGuess == 10) {
-        postAlert("Last chance!!!");
-      }
-      if (currentGuess > 10) {
-        postAlert("You ran out of attempts");
-        disableSubmit();
-        floatAlert(d);
-        postPrompt("Would you like to play again?", null, null);
-      }
-    }
-
-  });//end click
-});//end documentready
-
-//--------------------------
-// Function definitions
-computerGuess = function(){
- var digitOne = Math.floor(Math.random()*10);
- var digitTwo;
- var digitThree;
- do {
-   digitTwo = Math.floor(Math.random()*10);
- } while (digitTwo == digitOne);
-  do {
-   digitThree = Math.floor(Math.random()*10);  
-  } while (digitThree == digitOne || digitThree == digitTwo);
-  return [digitOne, digitTwo, digitThree];
-}
-postAlert = function(someText){
-  alert(someText);
-}
-disableSubmit = function() {
-  $("#process").disabled = true;
-}
-floatAlert = function(share) {
-  alert(share);
-}
-postPrompt = function(someText, result1, result2){
-  var answer =prompt(someText);
-  if (answer[0].toUpperCase() == "Y") {
-    location.reload();
-  } else{
-    location.assign("http://www.hampsterdance.com/classics/interactivedance.htm");
-  }
-    document.reload();
-
-}
-gameEnd = function(){
-  console.log("gameEnd did fire");
-  if (g[0] == d[0] && g[1] == d[1] && g[2] == d[2]) {
-    winOrLose = true;
-    return true;
-  } else {
-    currentGuess++; 
-    $("#remaining").html(11 - currentGuess);
-    if (currentGuess > 10) {
-      winOrLose = false;
-      return true;
-    }
-  }
-}
-gameEndAlert = function(didWin){
-  if (didWin) {
-    alert("You've figured it out");
-  } else {
-    alert("You'll never beat me");
-    //reveal the number
-  }
-    playAgain(); 
 }
 playAgain = function(){
-  replay = prompt("Would you like to play again?");
-  if (replay[0].toUpperCase() == "Y") {
-    location.reload();
+  setTimeout(function() {
+    replay = prompt("Would you like to play again?"); }, 1600);
+    setTimeout(function() { nextStep(replay); }, 1800);
+
+}
+nextStep = function(response){
+  if (response[0].toUpperCase() == "Y") {
+  location.reload();
+  } else {
+    location.assign("http://www.hampsterdance.com/classics/interactivedance.htm");
   }
+}
+function myFunction() {
+    setTimeout(function(){ prompt("Hello"); }, 1000);
 }
